76279c21fc98ac10311c431a0dc663811af0cf53
dchicchon <danielchicchon@gmail.com>
2021-12-14 16:33:23 -0500

    update styling for soda machine using svg. added ability to download soda json

--- client/src/components/SodaMachine/index.jsx
+++ client/src/components/SodaMachine/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { apiBuySoda, apiGetSodas } from '../../utils/api';
+import SodaMachineIcon from '../SodaMachineIcon';
 import './style.css';
 
 const SodaLabel = function SodaLabel({ soda, buySoda }) {
@@ -31,36 +32,44 @@ const SodaLabel = function SodaLabel({ soda, buySoda }) {
 
 const SodaMachine = function SodaMachine() {
     const [sodas, setSodas] = useState([]);
-
     const setNewSodas = async () => {
         const apiSodas = await apiGetSodas();
         // console.log(apiSodas)
         setSodas(apiSodas);
     };
+
+    const downloadJSON = async (result) => {
+      const fileName = result.label;
+      const json = JSON.stringify(result);
+      const blob = new Blob([json], { type: 'application/json' });
+      const href = await URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = href;
+      a.download = `${fileName}.json`;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    };
+
     const buySoda = async (id) => {
         const result = await apiBuySoda(id); // based on result, download json;
-        const fileName = result.label;
-        const json = JSON.stringify(result);
-        const blob = new Blob([json], { type: 'application/json' });
-        const href = await URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = href;
-        a.download = `${fileName}.json`;
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
+        downloadJSON(result);
         setNewSodas();
     };
+
     useEffect(() => {
         setNewSodas();
     }, []);
 
     return (
       <div id="soda-machine">
-        <h4>Soda Machine</h4>
-        {sodas.length && sodas.map((soda) => (
-          <SodaLabel key={soda.id} soda={soda} buySoda={buySoda} />
+        <SodaMachineIcon />
+        <div id="soda-selection">
+          {sodas.length && sodas.map((soda) => (
+            <SodaLabel key={soda.id} soda={soda} buySoda={buySoda} />
             ))}
+        </div>
+
       </div>
     );
 };
---
+++ client/src/components/SodaMachineIcon/index.jsx
@@ -0,0 +1,290 @@
+import React from 'react';
+import './style.css';
+
+const SodaMachineIcon = function SodaMachineIcon() {
+  return (
+    <div className="svg-wrapper">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="100%"
+        height="100%"
+        viewBox="0 0 500 500"
+        // xmlSpace="preserve"
+      >
+        <path
+          style={{
+        stroke: '#000',
+        strokeWidth: 1,
+        strokeDasharray: 'none',
+        strokeLinecap: 'butt',
+        strokeDashoffset: 0,
+        strokeLinejoin: 'miter',
+        strokeMiterlimit: 4,
+        fill: '#0970b5',
+        fillRule: 'nonzero',
+        opacity: 1,
+      }}
+          vectorEffect="non-scaling-stroke"
+          transform="matrix(1.03 0 0 1.52 250 250)"
+          d="M-146.398-104.167c0-23.323 27.806-42.23 62.105-42.23H84.293c34.3 0 62.105 18.907 62.105 42.23v208.334c0 23.323-27.806 42.23-62.105 42.23H-84.293c-34.3 0-62.105-18.907-62.105-42.23z"
+          strokeLinecap="round"
+        />
+        <path
+          style={{
+        stroke: '#50d13d',
+        strokeWidth: 0,
+        strokeDasharray: 'none',
+        strokeLinecap: 'butt',
+        strokeDashoffset: 0,
+        strokeLinejoin: 'miter',
+        strokeMiterlimit: 4,
+        fill: '#fff312',
+        fillRule: 'nonzero',
+        opacity: 1,
+      }}
+          vectorEffect="non-scaling-stroke"
+          transform="matrix(.13 .09 -2.31 3.56 331.3 301.69)"
+          d="M-50-50H50V50H-50z"
+        />
+        <path
+          style={{
+        stroke: '#50d13d',
+        strokeWidth: 0,
+        strokeDasharray: 'none',
+        strokeLinecap: 'butt',
+        strokeDashoffset: 0,
+        strokeLinejoin: 'miter',
+        strokeMiterlimit: 4,
+        fill: '#fff312',
+        fillRule: 'nonzero',
+        opacity: 1,
+      }}
+          vectorEffect="non-scaling-stroke"
+          transform="matrix(.13 .09 -1.19 1.84 124.26 109.81)"
+          d="M-50-50H50V50H-50z"
+        />
+        <path
+          style={{
+        stroke: '#50d13d',
+        strokeWidth: 0,
+        strokeDasharray: 'none',
+        strokeLinecap: 'butt',
+        strokeDashoffset: 0,
+        strokeLinejoin: 'miter',
+        strokeMiterlimit: 4,
+        fill: '#fff312',
+        fillRule: 'nonzero',
+        opacity: 1,
+      }}
+          vectorEffect="non-scaling-stroke"
+          transform="matrix(.13 .09 -1.44 2.22 368.3 372.48)"
+          d="M-50-50H50V50H-50z"
+        />
+        <path
+          style={{
+        stroke: '#50d13d',
+        strokeWidth: 0,
+        strokeDasharray: 'none',
+        strokeLinecap: 'butt',
+        strokeDashoffset: 0,
+        strokeLinejoin: 'miter',
+        strokeMiterlimit: 4,
+        fill: '#fff312',
+        fillRule: 'nonzero',
+        opacity: 1,
+      }}
+          vectorEffect="non-scaling-stroke"
+          transform="matrix(.13 .09 -2.89 4.44 253.59 234.97)"
+          d="M-50-50H50V50H-50z"
+        />
+        <path
+          style={{
+        stroke: '#000',
+        strokeWidth: 1,
+        strokeDasharray: 'none',
+        strokeLinecap: 'butt',
+        strokeDashoffset: 0,
+        strokeLinejoin: 'miter',
+        strokeMiterlimit: 4,
+        fill: 'none',
+        fillRule: 'nonzero',
+        opacity: 1,
+      }}
+          vectorEffect="non-scaling-stroke"
+          transform="matrix(5.54 0 0 1.52 250 398.23)"
+          d="M-14.154-14.154h28.308v28.308h-28.308z"
+          strokeLinecap="round"
+        />
+        <path
+          style={{
+        stroke: '#000',
+        strokeWidth: 1,
+        strokeDasharray: 'none',
+        strokeLinecap: 'butt',
+        strokeDashoffset: 0,
+        strokeLinejoin: 'miter',
+        strokeMiterlimit: 4,
+        fill: '#bcb4b2',
+        fillRule: 'nonzero',
+        opacity: 1,
+      }}
+          vectorEffect="non-scaling-stroke"
+          transform="matrix(5.52 0 0 1.26 250 398.93)"
+          d="M-12.916-12.916h25.832v25.832h-25.832z"
+          strokeLinecap="round"
+        />
+        <g>
+          <text
+            xmlSpace="preserve"
+            fontFamily="Permanent Marker"
+            fontSize={30}
+            style={{
+          stroke: 'none',
+          strokeWidth: 1,
+          strokeDasharray: 'none',
+          strokeLinecap: 'butt',
+          strokeDashoffset: 0,
+          strokeLinejoin: 'miter',
+          strokeMiterlimit: 4,
+          fill: '#fff',
+          fillRule: 'nonzero',
+          opacity: 1,
+          whiteSpace: 'pre',
+        }}
+            transform="matrix(1.33 0 0 1.33 252.85 93.46)"
+          >
+            <tspan x={-61.41} y={-10.24}>
+              Itential
+            </tspan>
+            <tspan x={-61.41} y={29.09}>
+              Drinks
+            </tspan>
+          </text>
+        </g>
+        <path
+          style={{
+        stroke: '#000',
+        strokeWidth: 0,
+        strokeDasharray: 'none',
+        strokeLinecap: 'butt',
+        strokeDashoffset: 0,
+        strokeLinejoin: 'miter',
+        strokeMiterlimit: 4,
+        fill: '#fff',
+        fillRule: 'nonzero',
+        opacity: 1,
+      }}
+          vectorEffect="non-scaling-stroke"
+          transform="translate(251.61 12.92)"
+          d="M-93.11 13.612H93.11v-27.224H-93.11v27.224"
+        />
+        <path
+          style={{
+        stroke: '#000',
+        strokeWidth: 0,
+        strokeDasharray: 'none',
+        strokeLinecap: 'butt',
+        strokeDashoffset: 0,
+        strokeLinejoin: 'miter',
+        strokeMiterlimit: 4,
+        fill: '#fff',
+        fillRule: 'nonzero',
+        opacity: 1,
+      }}
+          vectorEffect="non-scaling-stroke"
+          transform="matrix(0 2.19 -3.3 0 446.33 247.99)"
+          d="M-93.11 13.612H93.11v-27.224H-93.11v27.224"
+        />
+        <path
+          style={{
+        stroke: '#000',
+        strokeWidth: 0,
+        strokeDasharray: 'none',
+        strokeLinecap: 'butt',
+        strokeDashoffset: 0,
+        strokeLinejoin: 'miter',
+        strokeMiterlimit: 4,
+        fill: '#fff',
+        fillRule: 'nonzero',
+        opacity: 1,
+      }}
+          vectorEffect="non-scaling-stroke"
+          transform="matrix(2.19 0 0 .86 256.82 485.27)"
+          d="M-93.11 13.612H93.11v-27.224H-93.11v27.224"
+        />
+        <path
+          style={{
+        stroke: '#000',
+        strokeWidth: 0,
+        strokeDasharray: 'none',
+        strokeLinecap: 'butt',
+        strokeDashoffset: 0,
+        strokeLinejoin: 'miter',
+        strokeMiterlimit: 4,
+        fill: '#fff',
+        fillRule: 'nonzero',
+        opacity: 1,
+      }}
+          vectorEffect="non-scaling-stroke"
+          transform="matrix(0 2.19 -3.57 0 49.29 240.75)"
+          d="M-93.11 13.612H93.11v-27.224H-93.11v27.224"
+        />
+        <path
+          style={{
+        stroke: '#000',
+        strokeWidth: 0,
+        strokeDasharray: 'none',
+        strokeLinecap: 'butt',
+        strokeDashoffset: 0,
+        strokeLinejoin: 'miter',
+        strokeMiterlimit: 4,
+        fill: '#fff',
+        fillRule: 'nonzero',
+        opacity: 1,
+      }}
+          vectorEffect="non-scaling-stroke"
+          transform="matrix(1.07 0 0 1.07 389.44 26.87)"
+          d="m2.931-19.813-19.2 29.452 12.064 10.174L16.27-11z"
+        />
+        <path
+          style={{
+        stroke: '#000',
+        strokeWidth: 0,
+        strokeDasharray: 'none',
+        strokeLinecap: 'butt',
+        strokeDashoffset: 0,
+        strokeLinejoin: 'miter',
+        strokeMiterlimit: 4,
+        fill: '#fff',
+        fillRule: 'nonzero',
+        opacity: 1,
+      }}
+          vectorEffect="non-scaling-stroke"
+          transform="matrix(1.13 0 0 1.13 110.35 453.46)"
+          d="M-8.982-.344 4.663 9.357l4.32-7.75L-1.982-9.358z"
+        />
+        <path
+          style={{
+        stroke: '#000',
+        strokeWidth: 1,
+        strokeDasharray: 'none',
+        strokeLinecap: 'butt',
+        strokeDashoffset: 0,
+        strokeLinejoin: 'miter',
+        strokeMiterlimit: 4,
+        fill: '#ddeaff',
+        fillRule: 'nonzero',
+        opacity: 1,
+      }}
+          vectorEffect="non-scaling-stroke"
+          transform="matrix(2.35 0 0 .39 254.35 186.78)"
+          d="M-50-.072C-50-27.647-46.325-50-41.791-50h83.582C46.325-50 50-27.647 50-.072v.144C50 27.647 46.325 50 41.791 50h-83.582C-46.325 50-50 27.647-50 .072z"
+          strokeLinecap="round"
+        />
+      </svg>
+    </div>
+
+);
+};
+
+export default SodaMachineIcon;
