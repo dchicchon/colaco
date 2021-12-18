34e1d8247c16964372c2a56e3ec6d9e79dd1f33b
Daniel Chicchon <danielchicchon@Daniels-MacBook-Air.local>
2021-12-17 20:49:18 -0500

    rearrage app.js routes e.g. move app.use(routes) prior to app.get for react build

--- server/api/app.js
+++ server/api/app.js
@@ -8,18 +8,14 @@ const routes = require('./routes');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../interface/build')));
-} else {
-  app.get('/', (req, res) => {
-    res.send('Welcome to the development server');
-  });
 }
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../interface/build/index.html'));
 });
-app.use(routes);
 
 module.exports = app;
