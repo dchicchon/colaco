f8d2d6f3231f2942b4b446268efffaf1d9677f0c
Daniel Chicchon <danielchicchon@Daniels-MacBook-Air.local>
2021-12-17 20:36:13 -0500

    rename folder of api-interface to interface and did not update server; update import name;

--- server/api/app.js
+++ server/api/app.js
@@ -10,16 +10,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../api-interface/build')));
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../api-interface/build/index.html'));
-  });
+  app.use(express.static(path.join(__dirname, '../interface/build')));
 } else {
   app.get('/', (req, res) => {
     res.send('Welcome to the development server');
   });
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../interface/build/index.html'));
+});
 app.use(routes);
 
 module.exports = app;
