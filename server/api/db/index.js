0a7810fe41a3aebc5e4ddfd031132acfb71a67b9
Daniel Chicchon <danielchicchon@Daniels-MacBook-Air.local>
2021-12-17 21:03:59 -0500

    adding jawsdb url to database in api

--- server/api/db/index.js
+++ server/api/db/index.js
@@ -1,15 +1,20 @@
 const { Sequelize } = require('sequelize');
 const { isTest } = require('../utils/misc');
 
-const db = isTest()
-  ? new Sequelize({
-    logging: false,
-    dialect: 'sqlite',
-    storage: './testStorage.sqlite',
-  })
-  : new Sequelize({
-    logging: false,
-    dialect: 'sqlite',
-    storage: './storage.sqlite',
-  });
-module.exports = db;
+let db;
+if (process.env.JAWSDB_URL) {
+  db = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  db = isTest()
+    ? new Sequelize({
+      logging: false,
+      dialect: 'sqlite',
+      storage: './testStorage.sqlite',
+    })
+    : new Sequelize({
+      logging: false,
+      dialect: 'sqlite',
+      storage: './storage.sqlite',
+    });
+  module.exports = db;
+}
