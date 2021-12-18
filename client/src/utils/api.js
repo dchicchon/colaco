3e736d6893d8250f64fe9dd52e3d55250ddadcfb
Daniel Chicchon <danielchicchon@Daniels-MacBook-Air.local>
2021-12-17 21:20:24 -0500

    update package json to fix airbnb issue

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
+const BASEURL = process.env.BASE_URL;
+
 export const apiGetSodas = async () => {
-  const result = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/sodas`);
+  const result = await axios.get(`${BASEURL}/api/sodas`);
   return result.data;
 };
 
 export const apiBuySoda = async (id) => {
-  const result = await axios.put(`${process.env.REACT_APP_BASE_URL}/api/sodas`, { id });
+  const result = await axios.put(`${BASEURL}/api/sodas`, { id });
   return result.data;
 };
