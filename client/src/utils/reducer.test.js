8a7fbe2760e2b3a660c6fe7eb2a405790fe2bab6
Daniel Chicchon <danielchicchon@Daniels-MacBook-Air.local>
2021-12-19 11:28:21 -0500

    add additional expect to reducer test

--- client/src/utils/reducer.test.js
+++ client/src/utils/reducer.test.js
@@ -1,17 +1,24 @@
 import { reducer } from './reducer';
-import { ADD_MESSAGE } from './actions';
+import { ADD_MESSAGE, REMOVE_MESSAGE } from './actions';
 
 describe('Testing Reducer', () => {
-    test('should add a message to state', () => {
+    test('should add a message to state and remove it', () => {
         const state = {
             messages: [],
         };
-        const action = {
+        const addAction = {
             type: ADD_MESSAGE,
             payload: 'Test Message',
         };
 
-        const updatedState = reducer(state, action);
+        const updatedState = reducer(state, addAction);
+        const idToDelete = updatedState.messages[0].id;
         expect(updatedState.messages.length).toBe(1);
+        const deleteAction = {
+            type: REMOVE_MESSAGE,
+            payload: { id: idToDelete },
+        };
+        const updatedState2 = reducer(updatedState, deleteAction);
+        expect(updatedState2.messages.length).toBe(0);
     });
 });
