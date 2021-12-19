008b1d3a1d7001f54bb84501a9ef5f89f4c7eec4
Daniel Chicchon <danielchicchon@Daniels-MacBook-Air.local>
2021-12-19 11:00:39 -0500

    added test to click button. will add a kill switch to server but not working at the moment

--- client/src/__e2e__/e2e.test.jsx
+++ client/src/__e2e__/e2e.test.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import SodaMachine from '../components/SodaMachine';
 
 describe('e2e test', () => {
-    test('It runs a successful test', async () => {
-        expect(1 + 1).toBe(2);
-    });
-    test('It buys a soda and gets a response', async () => {
+    test('It can find a soda from the server', async () => {
         render(<SodaMachine />);
         await waitFor(() => {
             expect(screen.getAllByRole('document')[0]).toHaveTextContent('Fizz');
         });
+    });
+    test('It can buy a soda', async () => {
+        render(<SodaMachine />);
+        await waitFor(() => {
+            expect(screen.getAllByRole('document')[0]).toBeTruthy();
+        });
+        userEvent.click(screen.getAllByRole('button')[0]);
+        // expect(screen.getByText('Purchased')).toBeTruthy();
     });
 });
