d0e462defd21c9ced404a41ba093baa725043adb
Daniel Chicchon <danielchicchon@gmail.com>
2023-06-28 16:07:15 -0700

    update test scripts

--- client/src/components/SodaMachineIcon/index.jsx
+++ client/src/components/SodaMachineIcon/index.jsx
@@ -12,20 +12,20 @@ const SodaMachineIcon = function SodaMachineIcon() {
       >
         <g
           style={{
-        display: 'inline',
-      }}
+            display: 'inline',
+          }}
         >
           <rect
             style={{
-          fill: '#502d16',
-          fillOpacity: 1,
-          fillRule: 'evenodd',
-          stroke: '#000',
-          strokeWidth: 3.17028,
-          strokeMiterlimit: 4,
-          strokeDasharray: 'none',
-          strokeOpacity: 1,
-        }}
+              fill: '#502d16',
+              fillOpacity: 1,
+              fillRule: 'evenodd',
+              stroke: '#000',
+              strokeWidth: 3.17028,
+              strokeMiterlimit: 4,
+              strokeDasharray: 'none',
+              strokeOpacity: 1,
+            }}
             width={40.852}
             height={307.653}
             x={174.13}
@@ -35,53 +35,53 @@ const SodaMachineIcon = function SodaMachineIcon() {
           />
           <path
             style={{
-          fill: '#0ebbdc',
-          fillOpacity: 1,
-          fillRule: 'evenodd',
-          stroke: '#000',
-          strokeWidth: 0.696861,
-          strokeMiterlimit: 4,
-          strokeDasharray: 'none',
-          strokeOpacity: 1,
-        }}
+              fill: '#0ebbdc',
+              fillOpacity: 1,
+              fillRule: 'evenodd',
+              stroke: '#000',
+              strokeWidth: 0.696861,
+              strokeMiterlimit: 4,
+              strokeDasharray: 'none',
+              strokeOpacity: 1,
+            }}
             transform="rotate(-90)"
             d="M-31.472-30.003h15.09v34.14h-15.09zM-63.047-30.003h15.09v34.14h-15.09zM-94.621-30.003h15.09v34.14h-15.09zM-126.196-30.003h15.09v34.14h-15.09zM-157.77-30.003h15.09v34.14h-15.09zM-15.685-12.585h15.09v34.14h-15.09zM-31.472 4.834h15.09v34.14h-15.09zM-47.259-12.585h15.09v34.14h-15.09zM-63.047 4.834h15.09v34.14h-15.09zM-78.834-12.585h15.09v34.14h-15.09zM-94.621 4.834h15.09v34.14h-15.09zM-110.408-12.585h15.09v34.14h-15.09zM-126.196 4.834h15.09v34.14h-15.09zM-141.983-12.585h15.09v34.14h-15.09zM-157.77 4.834h15.09v34.14h-15.09zM-173.557-12.585h15.09v34.14h-15.09zM-15.685 22.252h15.09v34.14h-15.09zM-31.472 39.671h15.09v34.14h-15.09zM-47.259 22.252h15.09v34.14h-15.09zM-63.047 39.671h15.09v34.14h-15.09zM-78.834 22.252h15.09v34.14h-15.09zM-94.621 39.671h15.09v34.14h-15.09zM-110.408 22.252h15.09v34.14h-15.09zM-126.196 39.671h15.09v34.14h-15.09zM-141.983 22.252h15.09v34.14h-15.09zM-157.77 39.671h15.09v34.14h-15.09zM-173.557 22.252h15.09v34.14h-15.09zM-15.685 57.09h15.09v34.14h-15.09zM-31.472 74.508h15.09v34.14h-15.09zM-47.259 57.09h15.09v34.14h-15.09zM-63.047 74.508h15.09v34.14h-15.09zM-78.834 57.09h15.09v34.14h-15.09zM-94.621 74.508h15.09v34.14h-15.09zM-110.408 57.09h15.09v34.14h-15.09zM-126.196 74.508h15.09v34.14h-15.09zM-141.983 57.09h15.09v34.14h-15.09zM-157.77 74.508h15.09v34.14h-15.09zM-173.557 57.09h15.09v34.14h-15.09zM-15.685 91.927h15.09v34.14h-15.09zM-31.472 109.346h15.09v34.14h-15.09zM-47.259 91.927h15.09v34.14h-15.09zM-63.047 109.346h15.09v34.14h-15.09zM-78.834 91.927h15.09v34.14h-15.09zM-94.621 109.346h15.09v34.14h-15.09zM-110.408 91.927h15.09v34.14h-15.09zM-126.196 109.346h15.09v34.14h-15.09zM-141.983 91.927h15.09v34.14h-15.09zM-157.77 109.346h15.09v34.14h-15.09zM-173.557 91.927h15.09v34.14h-15.09zM-15.685 126.764h15.09v34.14h-15.09zM-31.472 144.183h15.09v34.14h-15.09zM-47.259 126.764h15.09v34.14h-15.09zM-63.047 144.183h15.09v34.14h-15.09zM-78.834 126.764h15.09v34.14h-15.09zM-94.621 144.183h15.09v34.14h-15.09zM-110.408 126.764h15.09v34.14h-15.09zM-126.196 144.183h15.09v34.14h-15.09zM-141.983 126.764h15.09v34.14h-15.09zM-157.77 144.183h15.09v34.14h-15.09zM-173.557 126.764h15.09v34.14h-15.09zM-15.685 161.601h15.09v34.14h-15.09zM-31.472 179.02h15.09v34.14h-15.09zM-47.259 161.601h15.09v34.14h-15.09zM-63.047 179.02h15.09v34.14h-15.09zM-78.834 161.601h15.09v34.14h-15.09zM-94.621 179.02h15.09v34.14h-15.09zM-110.408 161.601h15.09v34.14h-15.09zM-126.196 179.02h15.09v34.14h-15.09zM-141.983 161.601h15.09v34.14h-15.09zM-157.77 179.02h15.09v34.14h-15.09zM-173.557 161.601h15.09v34.14h-15.09zM-15.685 196.439h15.09v34.14h-15.09zM-31.472 213.857h15.09v34.14h-15.09zM-47.259 196.439h15.09v34.14h-15.09zM-63.047 213.857h15.09v34.14h-15.09zM-78.834 196.439h15.09v34.14h-15.09zM-94.621 213.857h15.09v34.14h-15.09zM-110.408 196.439h15.09v34.14h-15.09zM-126.196 213.857h15.09v34.14h-15.09zM-141.983 196.439h15.09v34.14h-15.09zM-157.77 213.857h15.09v34.14h-15.09zM-173.557 196.439h15.09v34.14h-15.09zM-15.685 231.276h15.09v34.14h-15.09zM-31.472 248.695h15.09v34.14h-15.09zM-47.259 231.276h15.09v34.14h-15.09zM-63.047 248.695h15.09v34.14h-15.09zM-78.834 231.276h15.09v34.14h-15.09zM-94.621 248.695h15.09v34.14h-15.09zM-110.408 231.276h15.09v34.14h-15.09zM-126.196 248.695h15.09v34.14h-15.09zM-141.983 231.276h15.09v34.14h-15.09zM-157.77 248.695h15.09v34.14h-15.09zM-173.557 231.276h15.09v34.14h-15.09zM-15.685 266.113h15.09v34.14h-15.09zM-31.472 283.532h15.09v34.14h-15.09zM-47.259 266.113h15.09v34.14h-15.09zM-63.047 283.532h15.09v34.14h-15.09zM-78.834 266.113h15.09v34.14h-15.09zM-94.621 283.532h15.09v34.14h-15.09zM-110.408 266.113h15.09v34.14h-15.09zM-126.196 283.532h15.09v34.14h-15.09zM-141.983 266.113h15.09v34.14h-15.09zM-157.77 283.532h15.09v34.14h-15.09zM-173.557 266.113h15.09v34.14h-15.09zM-15.685 300.951h15.09v34.14h-15.09zM-47.259 300.951h15.09v34.14h-15.09zM-78.834 300.951h15.09v34.14h-15.09zM-110.408 300.951h15.09v34.14h-15.09zM-141.983 300.951h15.09v34.14h-15.09zM-173.557 300.951h15.09v34.14h-15.09z"
           />
         </g>
         <g
           style={{
-        display: 'inline',
-      }}
+            display: 'inline',
+          }}
         >
           <text
             xmlSpace="preserve"
             style={{
-          fontStyle: 'normal',
-          fontWeight: 400,
-          fontSize: '10.5833px',
-          lineHeight: 1.25,
-          fontFamily: 'sans-serif',
-          fill: '#000',
-          fillOpacity: 1,
-          stroke: 'none',
-          strokeWidth: 0.264583,
-        }}
+              fontStyle: 'normal',
+              fontWeight: 400,
+              fontSize: '10.5833px',
+              lineHeight: 1.25,
+              fontFamily: 'sans-serif',
+              fill: '#000',
+              fillOpacity: 1,
+              stroke: 'none',
+              strokeWidth: 0.264583,
+            }}
             x={100.456}
             y={162.626}
           />
           <g transform="rotate(90 141.939 149.858)">
             <rect
               style={{
-            display: 'inline',
-            fill: '#0073a0',
-            fillOpacity: 1,
-            fillRule: 'evenodd',
-            stroke: '#000',
-            strokeWidth: 4.38558,
-            strokeMiterlimit: 4,
-            strokeDasharray: 'none',
-            strokeOpacity: 1,
-          }}
+                display: 'inline',
+                fill: '#0073a0',
+                fillOpacity: 1,
+                fillRule: 'evenodd',
+                stroke: '#000',
+                strokeWidth: 4.38558,
+                strokeMiterlimit: 4,
+                strokeDasharray: 'none',
+                strokeOpacity: 1,
+              }}
               width={169.088}
               height={112.96}
               x={15.723}
@@ -91,51 +91,51 @@ const SodaMachineIcon = function SodaMachineIcon() {
             />
             <path
               style={{
-            display: 'inline',
-            fill: '#0073a0',
-            fillOpacity: 1,
-            fillRule: 'evenodd',
-            stroke: '#000',
-            strokeWidth: 4.29411,
-            strokeMiterlimit: 4,
-            strokeDasharray: 'none',
-            strokeOpacity: 1,
-          }}
+                display: 'inline',
+                fill: '#0073a0',
+                fillOpacity: 1,
+                fillRule: 'evenodd',
+                stroke: '#000',
+                strokeWidth: 4.29411,
+                strokeMiterlimit: 4,
+                strokeDasharray: 'none',
+                strokeOpacity: 1,
+              }}
               d="M34.724 93.993a11.59 11.59 0 0 0-11.614 11.615v89.822a11.59 11.59 0 0 0 11.614 11.615h138.748a11.59 11.59 0 0 0 11.615-11.615v-89.822a11.59 11.59 0 0 0-11.615-11.615z"
             />
             <text
               xmlSpace="preserve"
               style={{
-            fontStyle: 'normal',
-            fontVariant: 'normal',
-            fontWeight: 700,
-            fontStretch: 'normal',
-            fontSize: '10.5833px',
-            lineHeight: 1.15,
-            fontFamily: 'STIXIntegralsSm',
-            InkscapeFontSpecification: "'STIXIntegralsSm Bold'",
-            display: 'inline',
-            fill: '#000700',
-            fillOpacity: 1,
-            stroke: 'none',
-            strokeWidth: 0.264583,
-          }}
+                fontStyle: 'normal',
+                fontVariant: 'normal',
+                fontWeight: 700,
+                fontStretch: 'normal',
+                fontSize: '10.5833px',
+                lineHeight: 1.15,
+                fontFamily: 'STIXIntegralsSm',
+                InkscapeFontSpecification: "'STIXIntegralsSm Bold'",
+                display: 'inline',
+                fill: '#000700',
+                fillOpacity: 1,
+                stroke: 'none',
+                strokeWidth: 0.264583,
+              }}
               x={-201.156}
               y={40.27}
               transform="rotate(-90)"
             >
               <tspan
                 style={{
-              fontStyle: 'normal',
-              fontVariant: 'normal',
-              fontWeight: 700,
-              fontStretch: 'normal',
-              fontFamily: 'monospace',
-              InkscapeFontSpecification: "'STIXIntegralsSm Bold'",
-              fill: '#000700',
-              fillOpacity: 1,
-              strokeWidth: 0.264583,
-            }}
+                  fontStyle: 'normal',
+                  fontVariant: 'normal',
+                  fontWeight: 700,
+                  fontStretch: 'normal',
+                  fontFamily: 'monospace',
+                  InkscapeFontSpecification: "'STIXIntegralsSm Bold'",
+                  fill: '#000700',
+                  fillOpacity: 1,
+                  strokeWidth: 0.264583,
+                }}
                 x={-201.156}
                 y={40.27}
               >
@@ -143,16 +143,16 @@ const SodaMachineIcon = function SodaMachineIcon() {
               </tspan>
               <tspan
                 style={{
-              fontStyle: 'normal',
-              fontVariant: 'normal',
-              fontWeight: 700,
-              fontStretch: 'normal',
-              fontFamily: 'monospace',
-              InkscapeFontSpecification: "'STIXIntegralsSm Bold'",
-              fill: '#000700',
-              fillOpacity: 1,
-              strokeWidth: 0.264583,
-            }}
+                  fontStyle: 'normal',
+                  fontVariant: 'normal',
+                  fontWeight: 700,
+                  fontStretch: 'normal',
+                  fontFamily: 'monospace',
+                  InkscapeFontSpecification: "'STIXIntegralsSm Bold'",
+                  fill: '#000700',
+                  fillOpacity: 1,
+                  strokeWidth: 0.264583,
+                }}
                 x={-201.156}
                 y={52.836}
               >
@@ -161,15 +161,15 @@ const SodaMachineIcon = function SodaMachineIcon() {
             </text>
             <rect
               style={{
-            display: 'inline',
-            fill: '#004366',
-            fillOpacity: 0.979757,
-            stroke: '#000',
-            strokeWidth: 3,
-            strokeMiterlimit: 4,
-            strokeDasharray: 'none',
-            strokeOpacity: 1,
-          }}
+                display: 'inline',
+                fill: '#004366',
+                fillOpacity: 0.979757,
+                stroke: '#000',
+                strokeWidth: 3,
+                strokeMiterlimit: 4,
+                strokeDasharray: 'none',
+                strokeOpacity: 1,
+              }}
               width={20.47}
               height={56.104}
               x={144.43}
@@ -179,26 +179,26 @@ const SodaMachineIcon = function SodaMachineIcon() {
             <text
               xmlSpace="preserve"
               style={{
-            fontStyle: 'normal',
-            fontWeight: 400,
-            fontSize: '2.11667px',
-            lineHeight: 1.25,
-            fontFamily: 'sans-serif',
-            display: 'inline',
-            fill: '#000',
-            fillOpacity: 1,
-            stroke: 'none',
-            strokeWidth: 0.264583,
-          }}
+                fontStyle: 'normal',
+                fontWeight: 400,
+                fontSize: '2.11667px',
+                lineHeight: 1.25,
+                fontFamily: 'sans-serif',
+                display: 'inline',
+                fill: '#000',
+                fillOpacity: 1,
+                stroke: 'none',
+                strokeWidth: 0.264583,
+              }}
               x={-129.464}
               y={128.515}
               transform="rotate(-90)"
             >
               <tspan
                 style={{
-              fontSize: '2.11667px',
-              strokeWidth: 0.264583,
-            }}
+                  fontSize: '2.11667px',
+                  strokeWidth: 0.264583,
+                }}
                 x={-129.464}
                 y={128.515}
               >
@@ -207,15 +207,15 @@ const SodaMachineIcon = function SodaMachineIcon() {
             </text>
             <rect
               style={{
-            display: 'inline',
-            fill: '#0ff',
-            fillOpacity: 1,
-            stroke: '#000',
-            strokeWidth: 4.09099,
-            strokeMiterlimit: 4,
-            strokeDasharray: 'none',
-            strokeOpacity: 1,
-          }}
+                display: 'inline',
+                fill: '#0ff',
+                fillOpacity: 1,
+                stroke: '#000',
+                strokeWidth: 4.09099,
+                strokeMiterlimit: 4,
+                strokeDasharray: 'none',
+                strokeOpacity: 1,
+              }}
               width={73.786}
               height={95.482}
               x={59.283}
@@ -224,15 +224,15 @@ const SodaMachineIcon = function SodaMachineIcon() {
             />
             <rect
               style={{
-            fill: '#004367',
-            fillOpacity: 1,
-            fillRule: 'evenodd',
-            stroke: '#000',
-            strokeWidth: 1.79203,
-            strokeMiterlimit: 4,
-            strokeDasharray: 'none',
-            strokeOpacity: 1,
-          }}
+                fill: '#004367',
+                fillOpacity: 1,
+                fillRule: 'evenodd',
+                stroke: '#000',
+                strokeWidth: 1.79203,
+                strokeMiterlimit: 4,
+                strokeDasharray: 'none',
+                strokeOpacity: 1,
+              }}
               width={4.733}
               height={15.067}
               x={137.842}
@@ -242,15 +242,15 @@ const SodaMachineIcon = function SodaMachineIcon() {
             <ellipse
               className="soda-svg-button-1"
               style={{
-            // fill: '#fff',
-            fillOpacity: 1,
-            fillRule: 'evenodd',
-            stroke: '#000',
-            strokeWidth: 1,
-            strokeMiterlimit: 4,
-            strokeDasharray: 'none',
-            strokeOpacity: 1,
-          }}
+                // fill: '#fff',
+                fillOpacity: 1,
+                fillRule: 'evenodd',
+                stroke: '#000',
+                strokeWidth: 1,
+                strokeMiterlimit: 4,
+                strokeDasharray: 'none',
+                strokeOpacity: 1,
+              }}
               cx={147.77}
               cy={110.794}
               rx={2.381}
@@ -259,16 +259,16 @@ const SodaMachineIcon = function SodaMachineIcon() {
             <ellipse
               className="soda-svg-button-2"
               style={{
-            display: 'inline',
-            // fill: '#fff',
-            fillOpacity: 1,
-            fillRule: 'evenodd',
-            stroke: '#000',
-            strokeWidth: 1,
-            strokeMiterlimit: 4,
-            strokeDasharray: 'none',
-            strokeOpacity: 1,
-          }}
+                display: 'inline',
+                // fill: '#fff',
+                fillOpacity: 1,
+                fillRule: 'evenodd',
+                stroke: '#000',
+                strokeWidth: 1,
+                strokeMiterlimit: 4,
+                strokeDasharray: 'none',
+                strokeOpacity: 1,
+              }}
               cx={147.77}
               cy={104.114}
               rx={2.381}
@@ -278,8 +278,7 @@ const SodaMachineIcon = function SodaMachineIcon() {
         </g>
       </svg>
     </div>
-
-);
+  );
 };
 
 export default SodaMachineIcon;
